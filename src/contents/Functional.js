ef736e223492f3d8979dc5a849918c5fc0cf89a9
unknown <vermakrishnaknt636@gmail.com>
2023-01-17 19:09:34 +0530

    first commit

---
+++ src/contents/Functional.js
@@ -0,0 +1,13 @@
+import './contents.css'
+function Functional(){
+
+    return(
+        <div className='contents'>
+            <h3>I was made using Functional Component</h3>
+            <p>This is done by using external CSS</p>
+            <p style={{color:' white'}}>This is done by using inline CSS</p>
+        </div>
+    )
+}
+
+export default Functional;
